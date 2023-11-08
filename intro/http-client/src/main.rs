355bcc3a7ae9844f68fae6b9dd6705c7efdc644b
David Antliff <david.antliff@gmail.com>
2023-11-09 12:17:32 +1300

    My solution to http-client.

--- intro/http-client/src/main.rs
+++ intro/http-client/src/main.rs
@@ -1,22 +1,19 @@
 #![no_std]
 #![no_main]
 
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, systimer::SystemTimer, Rng};
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer, Rng};
 
-use embedded_io::blocking::*;
 use embedded_svc::{
     ipv4::Interface,
     wifi::{AccessPointInfo, ClientConfiguration, Configuration, Wifi},
+    io::{Read, Write},
 };
 
 use esp_backtrace as _;
-use esp_println::{logger::init_logger, print, println};
-use esp_wifi::{
-    current_millis, initialize,
-    wifi::{utils::create_network_interface, WifiError, WifiMode},
-    wifi_interface::WifiStack,
-    EspWifiInitFor,
-};
+use esp_println::{print, println};
+use esp_wifi::wifi::utils::create_network_interface;
+use esp_wifi::wifi::{WifiError, WifiStaDevice};
+use esp_wifi::{current_millis, initialize, wifi_interface::WifiStack, EspWifiInitFor};
 use smoltcp::{
     iface::SocketStorage,
     wire::{IpAddress, Ipv4Address},
@@ -28,22 +25,37 @@ const PASSWORD: &str = env!("PASSWORD");
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let mut system = peripherals.SYSTEM.split();
+    let system = peripherals.SYSTEM.split();
     // Set clocks at maximum frequency
-    // let clocks =
+    let clocks = ClockControl::max(system.clock_control).freeze();
+
+    // Example: https://github.com/esp-rs/esp-wifi/blob/main/esp-wifi/examples/dhcp.rs
 
     // Create a timer and initialize the Wi-Fi
-    // let timer =
-    // let init =
+    let timer = timer::TimerGroup::new(peripherals.TIMG1, &clocks).timer0;
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    ).unwrap();
 
     // Configure Wifi
-    let (wifi, _) = peripherals.RADIO.split();
+    //let (wifi, _) = peripherals.RADIO.split();
+    let wifi = peripherals.WIFI;
     let mut socket_set_entries: [SocketStorage; 3] = Default::default();
     let (iface, device, mut controller, sockets) =
-        create_network_interface(&init, wifi, WifiMode::Sta, &mut socket_set_entries);
+        create_network_interface(&init, wifi, WifiStaDevice, &mut socket_set_entries).unwrap();
     let wifi_stack = WifiStack::new(iface, device, sockets, current_millis);
     // Create a Client with your Wi-Fi credentials and default configuration.
-    // let client_config = Configuration::Client(.....);
+    let client_config = Configuration::Client(
+        ClientConfiguration {
+            ssid: SSID.into(),
+            password: PASSWORD.into(),
+            ..Default::default()
+        }
+    );
     let res = controller.set_configuration(&client_config);
     println!("wifi_set_configuration returned {:?}", res);
 
@@ -102,13 +114,23 @@ fn main() -> ! {
         socket.work();
 
         // Open the socket
-        // socket
-        //     .open(....)
-        //     .unwrap();
-        // Write and flush the socket
-        // socket...
-        // socket...
+        println!("Open socket");
+        socket
+            //.open(IpAddress::Ipv4(Ipv4Address::new(142, 250, 185, 115)), 80)
+            .open(IpAddress::Ipv4(Ipv4Address::new(192, 168, 178, 1)), 80)
+            .unwrap();
 
+        // Write and flush the socket
+        println!("Write socket");
+        socket
+            .write(b"GET / HTTP/1.0\r\nHost: www.mobile-j.de\r\n\r\n")
+            .unwrap();
+        println!("Flush socket");
+        socket
+            .flush()
+            .unwrap();
+
+        println!("Wait");
         let wait_end = current_millis() + 20 * 1000;
         loop {
             let mut buffer = [0u8; 512];
