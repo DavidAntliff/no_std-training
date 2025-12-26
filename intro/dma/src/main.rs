de5e418426b7c01c5e868ffc5f5522254e11c68d
David Antliff <david@pitchblende.co.nz>
2025-12-26 14:29:38 +1300

    Solution to dma, for ESP32-C6-DevKitC-1.

--- intro/dma/src/main.rs
+++ intro/dma/src/main.rs
@@ -28,28 +28,62 @@ fn main() -> ! {
     let mosi = peripherals.GPIO4;
     let cs = peripherals.GPIO5;
 
+    // we need to create the DMA driver and get a channel
+    let dma_channel = peripherals.DMA_CH0;
+
+    // DMA transfers need descriptors and buffers
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+    let mut dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
     let mut spi = Spi::new(
         peripherals.SPI2,
         Config::default()
             .with_frequency(Rate::from_khz(100))
             .with_mode(Mode::_0),
     )
-    .unwrap()
-    .with_sck(sclk)
-    .with_mosi(mosi)
-    .with_miso(miso)
-    .with_cs(cs);
+        .unwrap()
+        .with_sck(sclk)
+        .with_mosi(mosi)
+        .with_miso(miso)
+        .with_cs(cs)
+        .with_dma(dma_channel);
 
     let delay = Delay::new();
+
+    // The data to send:
+    for (i, it) in dma_tx_buf.as_mut_slice().iter_mut().enumerate() {
+        *it = i as u8;
+    }
 
     loop {
         // ANCHOR: transfer
         // To transfer much larger amounts of data we can use DMA and
         // the CPU can even do other things while the transfer is in progress
-        let mut data = [0x01u8, 0x02, 0x03, 0x04];
-        spi.transfer(&mut data).unwrap();
+
+        // `dma_transfer` will move the driver and the buffers into the
+        // returned transfer.
+        let transfer = spi
+            .transfer(dma_rx_buf.len(), dma_rx_buf, dma_tx_buf.len(), dma_tx_buf)
+            .map_err(|e| e.0)
+            .unwrap();
+        // let mut data = [0x01u8, 0x02, 0x03, 0x04];
+        // spi.transfer(&mut data).unwrap();
         // ANCHOR_END: transfer
-        println!("{:x?}", data);
+
+        // wait for transfer to complete
+        while !transfer.is_done() {
+            print!(".");
+        }
+
+        // get the results of the transfer via .wait()
+        (spi, (dma_rx_buf, dma_tx_buf)) = transfer.wait();
+
+        println!();
+        println!("Received {:x?} .. {:x?}",
+                 &dma_rx_buf.as_slice()[..10],
+                 &dma_rx_buf.as_slice().last_chunk::<10>().unwrap()
+        );
 
         delay.delay_millis(2500u32);
     }
